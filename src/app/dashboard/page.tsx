72fabd507ac935981f521ab044fb82f961fc4146
Vinicius Perazza <vi_guepardo@hotmail.com>
2023-08-28 13:09:23 -0300

    started project

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import Button from "@/components/Button";
+
+export default function dashboard() {
+    return (
+        <section>
+            <Button>Hello</Button>
+        </section>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { db } from "@/lib/db"
+
+export default async function Home() {
+  return (
+    <main className="">
+      <h2>oi</h2>
+    </main>
+  )
+}
